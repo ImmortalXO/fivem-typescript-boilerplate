02c22b9d4fd0e261a91318cd10f96288f43e2662
ImmortalXO <claampers@gmail.com>
2022-04-24 02:17:44 -0400

    Updated import.

--- config.ts
+++ config.ts
@@ -1,8 +1,9 @@
 import * as CFX from "@nativewrappers/client";
+import { Cfg } from './typings'
 
 const currentResourceName = GetCurrentResourceName();
 
-const Config = JSON.parse(
+const Config: Cfg = JSON.parse(
   LoadResourceFile(currentResourceName, "config.json")
 );
 
