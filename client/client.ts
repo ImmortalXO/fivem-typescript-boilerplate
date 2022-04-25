3dd7ec21d8e6f96fdbd09dc30e1fe09691931ea4
zay <91941067+ImmortalXO@users.noreply.github.com>
2022-04-25 14:02:31 -0400

    Update client.ts

--- client/client.ts
+++ client/client.ts
@@ -3,8 +3,6 @@ import Config from "../config";
 
 on('onResourceStart', (resName: string) => {
   if (resName === GetCurrentResourceName()) {
-    Config.started = true
-    console.log(`Started: ${Config.started} on client.`);
     console.log(myRandomData)
     console.log('TypeScript boilerplate started!')
   }
