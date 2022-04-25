90b7b9eed61b2ceafdb66e45e16b0689f5147738
zay <91941067+ImmortalXO@users.noreply.github.com>
2022-04-25 12:18:46 -0400

    Create config.ts

---
+++ client/config.ts
@@ -0,0 +1,10 @@
+import * as CFX from "@nativewrappers/client";
+import Cfg from '../typings'
+
+const currentResourceName = GetCurrentResourceName();
+
+const Config: Cfg = JSON.parse(
+  LoadResourceFile(currentResourceName, "config.json")
+);
+
+export default Config;
