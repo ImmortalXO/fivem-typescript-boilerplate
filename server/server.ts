93fa8304bc499405e80f27e882f4c9cf9c930d57
zay <91941067+ImmortalXO@users.noreply.github.com>
2022-04-25 14:09:11 -0400

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import { myRandomData } from "./MyOther.server";
-import Config from '../config';
+import Config from './config';
 
 on("onResourceStart", (resName: string) => {
   if (resName === GetCurrentResourceName()) {
